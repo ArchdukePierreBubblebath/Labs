ab01171cecd4696b651e0c66f3c874a76e44d5a1
ansh <ansh@localhost>
2024-09-12 21:02:13 -0400

    updated lab progress

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,8 +3,66 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Skyrim belongs to the Nords!");
+		int aaacount = 0;
+		int i = 0;
+		char[] nucleotides = {'A', 'T', 'G', 'C'};
+		while (i < 1000) {
+		String dna = "";
+		int x = 0;
+		
+		while(x < 3) { 
+		int randomIndex = (int)(Math.random() * 4);
+		dna += nucleotides[randomIndex];
+		x +=1;
+		
+		}
+		if (dna.equals("AAA")){
+			aaacount +=1;
+		}
+		System.out.println(dna);
+		i+=1;
 
+		}
+		
+		System.out.println(aaacount);
+		int j = 0;
+		aaacount = 0;
+		while(j<1001) {
+		String newDNA = "";
+		int y = 0;
+		while(y<3) {
+		int random = (int)(Math.random() * 100);
+		if (random >-1 && random < 12) {
+			newDNA += "A";
+		
+		}
+		else if(random > 11 && random < 50) {
+			newDNA +="C";
+		}
+	
+		else if(random > 49 && random < 61) {
+			newDNA +="T";
+			
+		}
+		else if(random > 60 && random < 101) {
+			newDNA +="G";
+	
+		//System.out.println("meep");
+		
+		
+		}
+		y +=1;
+		
+		
+		
 	}
+		System.out.println(newDNA);
+		if (newDNA.equals("AAA")){
+			aaacount +=1;
+		}
+		j += 1;
+	}
+		System.out.println(aaacount);}}
 
-}
+
+
---
+++ src/Lab2.java
@@ -0,0 +1,41 @@
+
+public class Lab2 {
+	public static void main(String[]args) {
+
+	String[] Short_names = 
+		{ "A","R", "N", "D", "C", "Q", "E", 
+		"G",  "H", "I", "L", "K", "M", "F", 
+		"P", "S", "T", "W", "Y", "V" };
+	
+	String[] FULL_NAMES = 
+		{
+		"alanine","arginine", "asparagine", 
+		"aspartic acid", "cysteine",
+		"glutamine",  "glutamic acid",
+		"glycine" ,"histidine","isoleucine",
+		"leucine",  "lysine", "methionine", 
+		"phenylalanine", "proline", 
+		"serine","threonine","tryptophan", 
+		"tyrosine", "valine"};
+	
+	int x = 0;
+	String aa = "";
+	while(x<30) {
+		int randomIndex = (int)(Math.random() * 20);
+		aa += FULL_NAMES[randomIndex];
+		System.out.println(aa);
+		aa = "";
+		String a = Short_names[randomIndex];
+		//System.out.println(a);
+		String aString = System.console().readLine().toUpperCase();
+		if (aString.equals(a)) {
+			System.out.println("Correct!");
+			}
+		else {
+			System.out.println("You suck!");
+			break;
+		}
+		x++;
+		
+	}
+}}
