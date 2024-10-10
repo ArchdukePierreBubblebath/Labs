c3cda71751ac71c7d9f70ffdbe62aef683561a72
ansh <ansh@localhost>
2024-10-10 18:18:16 -0400

    Initial commit for fasta parser. At the moment, all it does is print the headers and sequences rather than making them into objects and appending them to a list.

---
+++ src/FastaSequence.java
@@ -0,0 +1,84 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FastaSequence {
+	private String header;
+	private String seq;
+	
+	public FastaSequence(String header,String seq) {
+		this.header = header;
+		this.seq = seq;
+	}
+	
+	public static List<FastaSequence> readFastaFile(String filepath) throws Exception
+	{List<FastaSequence> fastaList = new ArrayList<>();
+    BufferedReader reader = new BufferedReader(new FileReader(filepath));
+    String line;
+    StringBuilder str = new StringBuilder();
+    String header = null;
+    String seq = "";
+    while ((line = reader.readLine()) != null) {
+        //System.out.println(line);
+        if(line.startsWith(">")) {
+        	//System.out.println("Meep");
+        	if(header == null) {
+        		header = line;
+        		//System.out.println(line);
+        		
+        	}
+        	else if(header != null) {
+        		System.out.println(header);
+        		System.out.println(seq);
+        		header = line;
+        		seq = "";
+        	}
+ 
+        	}
+        else {
+        	seq +=line;
+        	//System.out.println(seq);
+        	//break;
+        }
+      
+        
+    }
+    System.out.println(header);
+    System.out.println(seq);
+	return fastaList;}
+	
+	public String getHeader()
+	{return header;}
+
+	// returns the Dna sequence of this FastaSequence
+	public String getSequence() 
+	{return seq;}
+	
+	// returns the number of G’s and C’s divided by the length of this sequence
+	public float getGCRatio()
+	{return 0;}
+
+	
+	public static void main(String[] args) throws Exception
+	{
+	     List<FastaSequence> fastaList = 
+		FastaSequence.readFastaFile(
+			"/Users/ansh/git/Labs/src/test.fasta");
+
+	     for( FastaSequence fs : fastaList)
+	     {
+	         System.out.println(fs.getHeader());
+	         System.out.println(fs.getSequence());
+	         System.out.println(fs.getGCRatio());
+	      }
+
+	     File myFile = new File("/Users/ansh/git/Labs/src/test.fasta");
+
+	     //writeTableSummary( fastaList,  myFile);
+	}
+
+
+
+}
